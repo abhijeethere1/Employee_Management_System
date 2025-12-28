57aa941e7bd4a2183505334379188ea04d841460
Abhijeet Singh <curiousabhijeet1@gmail.com>
2025-12-28 21:49:04 +0530

    add employee section to add edit and manage salary + leave

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,10 @@ import AdminSummary from "./components/dashboard/AdminSummary";
 import DepartmentList from "./components/department/DepartmentList";
 import AddDepartment from "./components/department/AddDepartment";
 import EditDepartment from "./components/department/EditDepartment";
+import List from "./components/employee/List";
+import Add from "./components/employee/Add";
+import View from "./components/employee/View";
+import Edit from "./components/employee/Edit";
 
 function App() {
   return (
@@ -37,6 +41,16 @@ function App() {
           <Route
             path="/admin-dashboard/department/:id"
             element={<EditDepartment />}
+          ></Route>
+          <Route path="/admin-dashboard/employees" element={<List />}></Route>
+          <Route path="/admin-dashboard/add-employee" element={<Add />}></Route>
+          <Route
+            path="/admin-dashboard/employee/:id"
+            element={<View />}
+          ></Route>
+          <Route
+            path="/admin-dashboard/employee/edit/:id"
+            element={<Edit />}
           ></Route>
         </Route>
         <Route
